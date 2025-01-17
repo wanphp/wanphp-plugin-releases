7e73f6817ce750c1cd52010982261e28945c5dd6
火子 <wanphp@outlook.com>
2025-01-17 18:10:13 +0800

    未配置SQLite使用mysql数据存放数据

--- src/Repositories/AuthorizationHeaderRepository.php
+++ src/Repositories/AuthorizationHeaderRepository.php
@@ -4,13 +4,20 @@ namespace Wanphp\Plugins\Releases\Repositories;
 
 use Wanphp\Libray\Mysql\BaseRepository;
 use Wanphp\Libray\Mysql\Database;
+use Wanphp\Libray\Slim\Setting;
 use Wanphp\Plugins\Releases\Domain\AuthorizationHeaderInterface;
 use Wanphp\Plugins\Releases\Entities\AuthorizationHeaderEntity;
 
 class AuthorizationHeaderRepository extends BaseRepository implements AuthorizationHeaderInterface
 {
-  public function __construct(Database $database)
+  public function __construct(Database $database, Setting $setting)
   {
+    // 使用SQLite存储发布数据，未配置数据存储到mysql
+    $options = $setting->get('releaseSqlite') ?? [];
+    if (!empty($options)) {
+      if (!file_exists($options['database'])) touch($options['database']);
+      $database = new Database($options);
+    }
     parent::__construct($database, self::TABLE_NAME, AuthorizationHeaderEntity::class);
   }
 }
--- src/Repositories/ReleasesRepository.php
+++ src/Repositories/ReleasesRepository.php
@@ -4,13 +4,20 @@ namespace Wanphp\Plugins\Releases\Repositories;
 
 use Wanphp\Libray\Mysql\BaseRepository;
 use Wanphp\Libray\Mysql\Database;
+use Wanphp\Libray\Slim\Setting;
 use Wanphp\Plugins\Releases\Domain\ReleasesInterface;
 use Wanphp\Plugins\Releases\Entities\ReleasesEntity;
 
 class ReleasesRepository extends BaseRepository implements ReleasesInterface
 {
-  public function __construct(Database $database)
+  public function __construct(Database $database, Setting $setting)
   {
+    // 使用SQLite存储发布数据，未配置数据存储到mysql
+    $options = $setting->get('releaseSqlite') ?? [];
+    if (!empty($options)) {
+      if (!file_exists($options['database'])) touch($options['database']);
+      $database = new Database($options);
+    }
     parent::__construct($database, self::TABLE_NAME, ReleasesEntity::class);
   }
 }
